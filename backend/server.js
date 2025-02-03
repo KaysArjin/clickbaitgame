403b0cd103c811191c4c56bad4881b3f1f107edc
Kays <kaysarjin@gmail.com>
2025-02-03 00:05:10 -0500

    Added logging for debugging

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const io = socketIo(server, {
 
 const PORT = process.env.PORT || 3000;
 
-// Store game state
 let players = [];
 let scores = {};
 let currentJudge = null;
@@ -23,7 +22,6 @@ let submittedLinks = [];
 io.on("connection", (socket) => {
     console.log("A player connected:", socket.id);
 
-    // Player joins the game
     socket.on("joinGame", (playerName) => {
         if (!players.includes(playerName)) {
             players.push(playerName);
@@ -37,16 +35,19 @@ io.on("connection", (socket) => {
         io.emit("updatePlayers", { players, scores, currentJudge });
     });
 
-    // Handle link submission
     socket.on("submitLink", (data) => {
         submittedLinks.push({ player: data.playerName, link: data.link });
 
         // Notify the judge how many links have been submitted
         io.to(currentJudge).emit("linksSubmitted", { count: submittedLinks.length });
 
+        console.log(`Total submitted links: ${submittedLinks.length}`)
+
         // If all players except the judge have submitted, pick a random link
         if (submittedLinks.length === players.length - 1) {
             const randomLink = submittedLinks[Math.floor(Math.random() * submittedLinks.length)];
+            
+            console.log(`Selected link for judge: ${randomLink.link}`);
             
             io.to(currentJudge).emit("showLink", { link: randomLink.link });
         }
